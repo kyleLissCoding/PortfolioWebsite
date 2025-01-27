3d7c078176d7dd909ee4d4cbf3ef39bb83427fa8
kyleliss <kliss@iu.edu>
2025-01-27 01:39:39 -0500

    adding updated info

--- js/typer.js
+++ js/typer.js
@@ -4,8 +4,11 @@ var i = 0;
 var txt1 = 'Software Engineer.';
 var txt2 = '</Frontend Developer>';
 var txt3 = 'Computer Science Student.';
+var txt4 = 'Full Stack Developer.';
+var txt5 = 'Problem Solver.';
+var txt6 = 'Backend Developer.';
 var textSpeed = 50;
-var randNum = Math.floor(Math.random()*3);
+var randNum = Math.floor(Math.random()*6); // Updated to include 5 options
 function typeWriter() {
   if(randNum==0){
     if (i < txt1.length) {
@@ -29,5 +32,25 @@ function typeWriter() {
         setTimeout(typeWriter, textSpeed);
     }
   }
-
-}
+  if(randNum==3){
+    if (i < txt4.length) {
+        document.getElementById("type").innerHTML += txt4.charAt(i);
+        i++;
+        setTimeout(typeWriter, textSpeed);
+    }
+  }
+  if(randNum==4){
+    if (i < txt5.length) {
+        document.getElementById("type").innerHTML += txt5.charAt(i);
+        i++;
+        setTimeout(typeWriter, textSpeed);
+    }
+  }
+  if(randNum==5){
+    if (i < txt6.length) {
+        document.getElementById("type").innerHTML += txt6.charAt(i);
+        i++;
+        setTimeout(typeWriter, textSpeed);
+    }
+  }
+} 
